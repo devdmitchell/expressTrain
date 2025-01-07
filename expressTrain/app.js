10d5e9a890db0f9ca183829ea43a7b9ad555346f
devdmitchell <dmitchell.prsvr@gmail.com>
2025-01-06 20:27:15 -0600

    C:/Program Files/Git/ route completed

--- expressTrain/app.js
+++ expressTrain/app.js
@@ -4,3 +4,13 @@ const logger = require('morgan')
 const app = express()
 app.use(logger('dev'))
 app.use(express.json())
+
+
+let store = [
+    { name: 'apple', 
+        price: 1.5 
+    },
+    { name: 'banana', 
+        price: 1.0 
+    }
+]
